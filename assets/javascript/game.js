898c0a67d6058b136d3dd1db2a88d1e4dce64165
matthew rybak <mrybak1991@gmail.com>
2019-06-09 17:58:07 -0400

    htmlandcss

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+
+var firstClick = 0;
+var secondClick = 0;
+
+   if(firstClick === 0) {
+    $("#pic").on("click", function firstClick() {
+        $("#pic").($(".challenger")    
+        );
+    });
+};
+    firstClick()
+});
